63953b8c8952aea5ebfbebb571f85ed20826fd85
Varrial <73936910+Varrial@users.noreply.github.com>
2022-06-07 14:27:48 +0200

    dodanie szablonów

--- home/models.py
+++ home/models.py
@@ -11,17 +11,25 @@ TYP = (
 class Osoba(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE, primary_key=True)
     data_urodzenia = models.DateField()
+    class Meta:
+        verbose_name_plural = 'Osoby'
 
 class Wybory(models.Model):
     nazwa = models.CharField(max_length=255)
     typ = models.CharField(choices=TYP, max_length=30)
+    class Meta:
+        verbose_name_plural = 'Wybory'
 
 class Uprawnieni(models.Model):
     id_wyborow = models.ForeignKey(Wybory, on_delete=models.CASCADE)
     pesel = models.ForeignKey(Osoba, on_delete=models.CASCADE)
     CzyZaglosowal = models.BooleanField(default=False)
+    class Meta:
+        verbose_name_plural = 'Uprawnieni'
 
 class Kandydaci(models.Model):
     id_wyborow = models.ForeignKey(Wybory, on_delete=models.CASCADE)
     pesel = models.ForeignKey(Osoba, on_delete=models.CASCADE)
     poparcie = models.PositiveSmallIntegerField()
+    class Meta:
+        verbose_name_plural = 'Kandydaci'
