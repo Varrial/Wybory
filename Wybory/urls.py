a745d7e282a1b471e9702359ba891f6ca432cd8a
Ksvabovsky <krzysioszw@gmail.com>
2022-06-10 22:35:51 +0200

    pełna obsługa funkcjonalności uzytkowników

--- Wybory/urls.py
+++ Wybory/urls.py
@@ -18,7 +18,7 @@ from django.urls import path, include
 
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls, name='admin'),
     path('', include('base.urls')),
     path('', include('django.contrib.auth.urls')),
     path('', include('members.urls')),
--- base/views.py
+++ base/views.py
@@ -1,9 +1,10 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
 from django.shortcuts import render, redirect
 from django.forms import ModelChoiceField, forms
 
 
-from .models import Wybory, Kandydaci, TypWyborow
+from .models import Wybory, Kandydaci, TypWyborow, Uprawnieni
 
 
 def home(request):
@@ -12,32 +13,54 @@ def home(request):
         q = ''
 
     if q != '':
-        wybory = Wybory.objects.filter(typ__typ=q)
+        if request.user.is_authenticated:
+            uprawnieni = Uprawnieni.objects.filter(id_wyborow__typ__typ=q, pesel=request.user)
+        else:
+            uprawnieni = Uprawnieni.objects.filter(id_wyborow__typ__typ=q)
+    elif request.user.is_authenticated:
+        uprawnieni = Uprawnieni.objects.filter(pesel=request.user)
     else:
-        wybory = Wybory.objects.all()
+        uprawnieni = Uprawnieni.objects.all()
+
     typy = TypWyborow.objects.all()
 
     context = {
-        'wybory': wybory,
+        'wybory': uprawnieni,
         'typy': typy,
     }
     return render(request, 'base/home.html', context)
 
+@login_required
 def wybory(request, pk):
     wybory = Wybory.objects.get(id=pk)
     kandydaci = Kandydaci.objects.filter(id_wyborow=pk)
+    uprawnieni = Uprawnieni.objects.filter(id_wyborow=wybory)
 
+    if not request.user.is_authenticated:   # jeżeli jest niezalogowany
+        valid = False
+    elif uprawnieni.filter(pesel=request.user).exists() and not uprawnieni.get(pesel=request.user).CzyZaglosowal:
+        # zalogowany jest uprawniony do wyborów i nie głosował
+        valid = True
+    else:
+        valid = False
+
+
+    valid = valid
     if request.method == 'POST':
         zaglosowany = request.POST.get('kandydat')
         item = kandydaci.get(pesel__username=zaglosowany)
-        item.poparcie += 1
+        item.poparcie += 1  # zwiekszenie poparcia
         item.save()
+        glosujacy = uprawnieni.get(id_wyborow=wybory, pesel=request.user)
+        glosujacy.CzyZaglosowal = True  # zmiana na zagłosowane
+        glosujacy.save()
         print(item.poparcie)
         return redirect('home')
 
     context = {
         'wybory': wybory,
         'kandydaci': kandydaci,
+        'valid': valid,
     }
     return render(request, 'base/wybory.html', context)
 
